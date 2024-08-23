274b6f9edf4b8e14a22a98a8d1d141ef0284b105
kapods22 <contact@kingdomanimaliapod.com>
2024-08-23 11:35:52 -0700

    Update main-script.js

--- website/main-script.js
+++ website/main-script.js
@@ -1,22 +1,20 @@
 // Remove href attribute from menu headers and <a> elements with no links
-//window.onload = () => {
-  let unlinkedAs = [
-    document.querySelectorAll("a[data-link-id='17627884']"),
-    document.querySelectorAll("a[data-link-id='18205969']"),
-    document.querySelectorAll("a[data-link-id='17627819']"),
-    document.querySelectorAll("a[data-page-link-id='15010016']"),
-    document.querySelectorAll("a[data-page-link-id='12029268']"),
-    document.querySelectorAll("a[data-page-link-id='12784877']"),
-    document.querySelectorAll("a[data-page-link-id='12514772']"),
-    document.querySelectorAll("a[href='']")
-  ];
-  
-  for (let i = 0; i < unlinkedAs.length; i++) {
-    for (let j = 0; j < unlinkedAs[i].length; j++) {
-     const styleAttr = document.createAttribute("style");
-     styleAttr.value = "cursor:pointer;";
-     unlinkedAs[i][j].removeAttribute("href");
-     unlinkedAs[i][j].setAttributeNode(styleAttr);
-    }
+let unlinkedAs = [
+  document.querySelectorAll("a[data-link-id='17627884']"),
+  document.querySelectorAll("a[data-link-id='18205969']"),
+  document.querySelectorAll("a[data-link-id='17627819']"),
+  document.querySelectorAll("a[data-page-link-id='15010016']"),
+  document.querySelectorAll("a[data-page-link-id='12029268']"),
+  document.querySelectorAll("a[data-page-link-id='12784877']"),
+  document.querySelectorAll("a[data-page-link-id='12514772']"),
+  document.querySelectorAll("a[href='']")
+];
+
+for (let i = 0; i < unlinkedAs.length; i++) {
+  for (let j = 0; j < unlinkedAs[i].length; j++) {
+   const styleAttr = document.createAttribute("style");
+   styleAttr.value = "cursor:pointer;";
+   unlinkedAs[i][j].removeAttribute("href");
+   unlinkedAs[i][j].setAttributeNode(styleAttr);
   }
-//}
+}
