eb7f2ca839d66447bc81f606f515858254830189
kapods22 <contact@kingdomanimaliapod.com>
2025-05-31 12:56:56 -0700

    Update main-script-initial.js
    Adding Player Links code

--- website/main-script-initial.js
+++ website/main-script-initial.js
@@ -1,109 +1,257 @@
-function displayPodroll(podcast) {
-  if (podcast == "KA") {
-    let podroll = document.getElementById("podroll");
-    let podcasts = [
-      {
-        title: "Amazing Wildlife: A San Diego Zoo Podcast",
-        author: "iHeartPodcasts",
-        artwork: "https://www.omnycontent.com/d/playlist/e73c998e-6e60-432f-8610-ae210140c5b1/22554c7c-336b-4dab-8602-ae3900375f03/f00b6583-9098-41fa-b61b-ae3900375f11/image.jpg",
-        url: "https://episodes.fm/1593442890"
-      },
-      {
-        title: "Earth Rangers",
-        author: "GZM Shows",
-        artwork: "https://megaphone.imgix.net/podcasts/f099b95e-6468-11ee-90cf-8b94eea21a5d/image/417b0aa0a83f0bd4e0cc1ef0f80aaa0a0d89749c30833a57a5b811305a6108e10a60992fe3f81b336711c06d4dd5bc17c6e038d3b44533ffefed20dd8a22a466.jpeg",
-        url: "https://episodes.fm/1408102830"
-      },
-      {
-        title: "Short Wave",
-        author: "NPR",
-        artwork: "https://media.npr.org/assets/img/2022/09/23/short-wave_tile_npr-network-01_sq-517382b4b8fd0ab48ea9c781253f9992eab733dc.jpg",
-        url: "https://episodes.fm/1482575855"
-      },
-      {
-        title: "Unexplainable",
-        author: "Vox",
-        artwork: "https://megaphone.imgix.net/podcasts/bf04a680-6afe-11eb-a86a-3f07259749a3/image/d510ec950f9a501094c11003381fcedc.jpg",
-        url: "https://episodes.fm/1554578197"
-      },
-      {
-        title: "Science Friday",
-        author: "Science Friday and WNYC Studios",
-        artwork: "https://image.simplecastcdn.com/images/ac8e2039-dfef-4938-b66a-c2f58f4b7599/c01bb987-b09c-4e1a-b8ba-5cd859951535/3000x3000/sciencefriday-wnycstudios-1400.jpg",
-        url: "https://episodes.fm/73329284"
-      },
-      {
-        title: "The Big Melt",
-        author: "GZM Shows",
-        artwork: "https://megaphone.imgix.net/podcasts/dab15b72-145b-11ee-9a1d-e30078842ba7/image/bigmelt-3000.jpg",
-        url: "https://episodes.fm/1501895003"
-      },
-      {
-        title: "How Wild",
-        author: "NPR",
-        artwork: "https://media.npr.org/assets/img/2024/08/01/how-wild-final-logo_sq-63107a48fb042203b2eb8c6340d874b75bd4bfc0.jpg",
-        url: "https://episodes.fm/1760612746"
-      },
-      {
-        title: "Six Minutes",
-        author: "GZM Shows",
-        artwork: "https://megaphone.imgix.net/podcasts/cb57fbec-6238-11ee-9b92-57dc4faf265f/image/8841a8c9bdf810e0d5b6b042ccd2893c067c8fb577e0a42a9d1427a3ae61b1c62cc580b902468f4437032a5537757059ec15b73b71d1f0f407b55cc02f0aef95.jpeg",
-        url: "https://episodes.fm/1348470106"
-      },
-      {
-        title: "Radiolab",
-        author: "WNYC Studies",
-        artwork: "https://image.simplecastcdn.com/images/3b6088d3-ed01-4c8f-b0af-bcbd72035bcf/48797471-8a28-49e4-95aa-80a28ffa6d82/3000x3000/radiolab-branded-logo-1400x1400.jpg",
-        url: "https://episodes.fm/152249110"
-      },
-      {
-        title: "The Big Fib",
-        author: "GZM Shows",
-        artwork: "https://megaphone.imgix.net/podcasts/3afde448-6469-11ee-90a2-076ec31b5eec/image/2f4874fcf8a29f366061215072c6b37e5fe344ac2e67575c2ae641baa67cbf843b499cc9e96d7c4f4de4251aa2f6f274aeabdb1e6b44ee1d2c2d00b547127f00.jpeg",
-        url: "https://episodes.fm/1348469682"
-      },
-      {
-        title: "The Natureverse",
-        author: "GZM Shows",
-        artwork: "https://megaphone.imgix.net/podcasts/f9ab4ce6-4b32-11ee-9167-4b0021a2d9b1/image/1aa00803dce1cc11a34d78ddf961021c01c9bec77448f9a3f9e1436a86cb320274334e6c15f25f8fb0179582a1079958018e2f53a14c379b20dd9dd571e54326.jpeg",
-        url: "https://episodes.fm/1423733399"
-      },
-      {
-        title: "The Show About Science",
-        author: "Nate | The Company Making Podcasts",
-        artwork: "https://image.simplecastcdn.com/images/a4f537ea-b9b1-4f48-8b35-26018cbfbf13/79950ba9-4199-4336-9ac1-c3a37b16a548/3000x3000/avatars-mz2tuarvohuuvzio-dhksjq-original.jpg",
-        url: "https://episodes.fm/1046413761"
-      },
-      {
-        title: "Tai Asks Why",
-        author: "CBC",
-        artwork: "https://www.cbc.ca/radio/podcasts/images/promo_TAIASKSWHY_2024.jpg",
-        url: "https://pod.link/1434443225"
-      },
-      {
-        title: "Cool Facts About Animals",
-        author: "Cool Facts About Animals Podcast",
-        artwork: "https://static.libsyn.com/p/assets/3/3/f/b/33fb1954c3c6034b/avatars-000394173162-wwrygq-original.jpg",
-        url: "https://episodes.fm/1334467437"
-      },
-      {
-        title: "Tumble Science Podcast for Kids",
-        author: "Tumble Media",
-        artwork: "https://megaphone.imgix.net/podcasts/26aa508c-d505-11ee-bb02-73230b38f27c/image/1c6523f5717fbcd9f8647bc4c72a3b57.jpg",
-        url: "https://episodes.fm/984771479"
-      }
-    ];
-    for (let podcast of podcasts) {
-      podroll.innerHTML +=
-        `<div class="podcast">
-          <a href="${podcast.url}" target="_blank">
-            <div class="artwork">
-              <img src="${podcast.artwork}">
-            </div>
-            <span class="title">${podcast.title}</span><br>
-            <span class="author">${podcast.author}</span>
-          </a>
-        </div>`;
+let podrolls = {
+  KA: [
+    {
+      title: "Amazing Wildlife: A San Diego Zoo Podcast",
+      author: "iHeartPodcasts",
+      artwork: "https://www.omnycontent.com/d/playlist/e73c998e-6e60-432f-8610-ae210140c5b1/22554c7c-336b-4dab-8602-ae3900375f03/f00b6583-9098-41fa-b61b-ae3900375f11/image.jpg",
+      url: "https://episodes.fm/1593442890"
+    },
+    {
+      title: "Earth Rangers",
+      author: "GZM Shows",
+      artwork: "https://megaphone.imgix.net/podcasts/f099b95e-6468-11ee-90cf-8b94eea21a5d/image/417b0aa0a83f0bd4e0cc1ef0f80aaa0a0d89749c30833a57a5b811305a6108e10a60992fe3f81b336711c06d4dd5bc17c6e038d3b44533ffefed20dd8a22a466.jpeg",
+      url: "https://episodes.fm/1408102830"
+    },
+    {
+      title: "Short Wave",
+      author: "NPR",
+      artwork: "https://media.npr.org/assets/img/2022/09/23/short-wave_tile_npr-network-01_sq-517382b4b8fd0ab48ea9c781253f9992eab733dc.jpg",
+      url: "https://episodes.fm/1482575855"
+    },
+    {
+      title: "Unexplainable",
+      author: "Vox",
+      artwork: "https://megaphone.imgix.net/podcasts/bf04a680-6afe-11eb-a86a-3f07259749a3/image/d510ec950f9a501094c11003381fcedc.jpg",
+      url: "https://episodes.fm/1554578197"
+    },
+    {
+      title: "Science Friday",
+      author: "Science Friday and WNYC Studios",
+      artwork: "https://image.simplecastcdn.com/images/ac8e2039-dfef-4938-b66a-c2f58f4b7599/c01bb987-b09c-4e1a-b8ba-5cd859951535/3000x3000/sciencefriday-wnycstudios-1400.jpg",
+      url: "https://episodes.fm/73329284"
+    },
+    {
+      title: "The Big Melt",
+      author: "GZM Shows",
+      artwork: "https://megaphone.imgix.net/podcasts/dab15b72-145b-11ee-9a1d-e30078842ba7/image/bigmelt-3000.jpg",
+      url: "https://episodes.fm/1501895003"
+    },
+    {
+      title: "How Wild",
+      author: "NPR",
+      artwork: "https://media.npr.org/assets/img/2024/08/01/how-wild-final-logo_sq-63107a48fb042203b2eb8c6340d874b75bd4bfc0.jpg",
+      url: "https://episodes.fm/1760612746"
+    },
+    {
+      title: "Six Minutes",
+      author: "GZM Shows",
+      artwork: "https://megaphone.imgix.net/podcasts/cb57fbec-6238-11ee-9b92-57dc4faf265f/image/8841a8c9bdf810e0d5b6b042ccd2893c067c8fb577e0a42a9d1427a3ae61b1c62cc580b902468f4437032a5537757059ec15b73b71d1f0f407b55cc02f0aef95.jpeg",
+      url: "https://episodes.fm/1348470106"
+    },
+    {
+      title: "Radiolab",
+      author: "WNYC Studies",
+      artwork: "https://image.simplecastcdn.com/images/3b6088d3-ed01-4c8f-b0af-bcbd72035bcf/48797471-8a28-49e4-95aa-80a28ffa6d82/3000x3000/radiolab-branded-logo-1400x1400.jpg",
+      url: "https://episodes.fm/152249110"
+    },
+    {
+      title: "The Big Fib",
+      author: "GZM Shows",
+      artwork: "https://megaphone.imgix.net/podcasts/3afde448-6469-11ee-90a2-076ec31b5eec/image/2f4874fcf8a29f366061215072c6b37e5fe344ac2e67575c2ae641baa67cbf843b499cc9e96d7c4f4de4251aa2f6f274aeabdb1e6b44ee1d2c2d00b547127f00.jpeg",
+      url: "https://episodes.fm/1348469682"
+    },
+    {
+      title: "The Natureverse",
+      author: "GZM Shows",
+      artwork: "https://megaphone.imgix.net/podcasts/f9ab4ce6-4b32-11ee-9167-4b0021a2d9b1/image/1aa00803dce1cc11a34d78ddf961021c01c9bec77448f9a3f9e1436a86cb320274334e6c15f25f8fb0179582a1079958018e2f53a14c379b20dd9dd571e54326.jpeg",
+      url: "https://episodes.fm/1423733399"
+    },
+    {
+      title: "The Show About Science",
+      author: "Nate | The Company Making Podcasts",
+      artwork: "https://image.simplecastcdn.com/images/a4f537ea-b9b1-4f48-8b35-26018cbfbf13/79950ba9-4199-4336-9ac1-c3a37b16a548/3000x3000/avatars-mz2tuarvohuuvzio-dhksjq-original.jpg",
+      url: "https://episodes.fm/1046413761"
+    },
+    {
+      title: "Tai Asks Why",
+      author: "CBC",
+      artwork: "https://www.cbc.ca/radio/podcasts/images/promo_TAIASKSWHY_2024.jpg",
+      url: "https://pod.link/1434443225"
+    },
+    {
+      title: "Cool Facts About Animals",
+      author: "Cool Facts About Animals Podcast",
+      artwork: "https://static.libsyn.com/p/assets/3/3/f/b/33fb1954c3c6034b/avatars-000394173162-wwrygq-original.jpg",
+      url: "https://episodes.fm/1334467437"
+    },
+    {
+      title: "Tumble Science Podcast for Kids",
+      author: "Tumble Media",
+      artwork: "https://megaphone.imgix.net/podcasts/26aa508c-d505-11ee-bb02-73230b38f27c/image/1c6523f5717fbcd9f8647bc4c72a3b57.jpg",
+      url: "https://episodes.fm/984771479"
     }
+  ]
+};
+let players = {
+  KA: [
+    {
+      name: "RSS Feed",
+      logo: "https://files.kapods.org/media/images/player-logos/rss-feed.png",
+      function: "copyRSSFeed('KA');"
+    },
+    {
+      name: "Apple Podcasts",
+      logo: "https://files.kapods.org/media/images/player-logos/apple-podcasts.png",
+      url: "https://podcasts.apple.com/us/podcast/kingdom-animalia-a-zoology-podcast-for-kids/id1648007256"
+    },
+    {
+      name: "Pocket Casts",
+      logo: "https://files.kapods.org/media/images/player-logos/pocket-casts.png",
+      url: "https://pca.st/qmy0f9f5"
+    },
+    {
+      name: "Overcast (Web)",
+      logo: "https://files.kapods.org/media/images/player-logos/overcast.png",
+      url: "https://overcast.fm/itunes1648007256"
+    },
+    {
+      name: "Overcast (App)",
+      logo: "https://files.kapods.org/media/images/player-logos/overcast.png",
+      function: "openOvercast();"
+    },
+    {
+      name: "iHeartRadio",
+      logo: "https://files.kapods.org/media/images/player-logos/iheartradio.png",
+      url: "https://iheart.com/podcast/102727303/"
+    },
+    {
+      name: "Castbox",
+      logo: "https://files.kapods.org/media/images/player-logos/castbox.png",
+      url: "https://castbox.fm/channel/Kingdom%3A-Animalia---A-Zoology-Podcast-for-Kids-id5187145?country=us"
+    },
+    {
+      name: "Podcast Addict",
+      logo: "https://files.kapods.org/media/images/player-logos/podcast-addict.png",
+      url: "https://podcastaddict.com/podcast/4104368"
+    },
+    {
+      name: "Goodpods",
+      logo: "https://files.kapods.org/media/images/player-logos/goodpods.png",
+      url: "https://goodpods.com/podcasts/kingdom-animalia-a-zoology-podcast-for-kids-217240"
+    },
+    {
+      name: "Castro",
+      logo: "https://files.kapods.org/media/images/player-logos/castro.png",
+      url: "https://castro.fm/itunes/1648007256"
+    },
+    {
+      name: "Listen Notes",
+      logo: "https://files.kapods.org/media/images/player-logos/listen-notes.png",
+      url: "https://www.listennotes.com/c/9c9dc7025e73431db2d246af8f210dd8/"
+    },
+    {
+      name: "Podcast Index",
+      logo: "https://files.kapods.org/media/images/player-logos/podcast-index.png",
+      url: "https://podcastindex.org/podcast/5710023"
+    },
+    {
+      name: "TuneIn",
+      logo: "https://files.kapods.org/media/images/player-logos/tunein.png",
+      url: "https://tunein.com/podcasts/Kingdom-Animalia---A-Zoology-Podcast-For-Kids-p2049490/?utm_content=p2049490&utm_medium=referral"
+    },
+    {
+      name: "Audible",
+      logo: "https://files.kapods.org/media/images/player-logos/audible.png",
+      url: "https://www.audible.com/podcast/Kingdom-Animalia-A-Zoology-Podcast-for-Kids/B0BG3NTRX5"
+    },
+    {
+      name: "Amazon Music",
+      logo: "https://files.kapods.org/media/images/player-logos/amazon-music.png",
+      url: "https://music.amazon.com/podcasts/13a5b620-14c0-4f02-8634-d815407abca7"
+    },
+    {
+      name: "Podbean",
+      logo: "https://files.kapods.org/media/images/player-logos/podbean.png",
+      url: "https://www.podbean.com/podcast-detail/uwac3-28a986/Kingdom-Animalia---A-Zoology-Podcast-for-Kids"
+    },
+    {
+      name: "Player FM",
+      logo: "https://files.kapods.org/media/images/player-logos/player-fm.png",
+      url: "https://player.fm/series/kingdom-animalia-3398814"
+    },
+    {
+      name: "JioSaavn",
+      logo: "https://files.kapods.org/media/images/player-logos/jiosaavn.png",
+      url: "https://www.jiosaavn.com/shows/kingdom-animalia-a-zoology-podcast-for-kids/1/sWUcMLubnlI_"
+    },
+    {
+      name: "Audacy",
+      logo: "https://files.kapods.org/media/images/player-logos/audacy.png",
+      url: "https://www.audacy.com/podcast/kingdom-animalia-a-zoology-podcast-for-kids-0c200"
+    },
+    {
+      name: "Deezer",
+      logo: "https://files.kapods.org/media/images/player-logos/deezer.png",
+      url: "https://www.deezer.com/us/show/5220947?deferredFl=1"
+    },
+    {
+      name: "Pandora",
+      logo: "https://files.kapods.org/media/images/player-logos/pandora.png",
+      url: "https://www.pandora.com/podcast/kingdom-animalia-a-zoology-podcast-for-kids/PC:1001074780"
+    },
+    {
+      name: "Podcast Republic",
+      logo: "https://files.kapods.org/media/images/player-logos/podcast-republic.png",
+      url: "https://www.podcastrepublic.net/podcast/1648007256"
+    },
+    {
+      name: "TrueFans",
+      logo: "https://files.kapods.org/media/images/player-logos/truefans.png",
+      url: "https://truefans.fm/kingdom-animalia-a-zoology-podcast-for-kids"
+    },
+    {
+      name: "Podchaser",
+      logo: "https://files.kapods.org/media/images/player-logos/podchaser.png",
+      url: "https://www.podchaser.com/podcasts/kingdom-animalia-4896812"
+    },
+    {
+      name: "iVoox",
+      logo: "https://files.kapods.org/media/images/player-logos/ivoox.png",
+      url: "https://www.ivoox.com/en/podcast-kingdom-animalia-a-zoology-podcast-for-kids_sq_f12267091_1.html"
+    }
+  ]
+};
+
+function displayPodroll(podcast) {
+  let podrollContainer = document.getElementById("podroll");
+  
+  for (let item of podrolls[podcast]) {
+    podrollContainer.innerHTML +=
+      `<div class="podcast">
+        <a href="${item.url}" target="_blank">
+          <div class="artwork">
+            <img src="${item.artwork}">
+          </div>
+          <span class="title">${item.title}</span><br>
+          <span class="author">${item.author}</span>
+        </a>
+      </div>`;
+  }
+}
+
+function copyRSSFeed(podcast) {
+  navigator.clipboard.writeText(feed(podcast));
+  alert(`The RSS Feed has been copied to your clipboard.`);
+}
+
+function displayPlayerLinks(podcast) {
+  let playerLinks = document.getElementById("player-links");
+  
+  for (let player of players[podcast]) {
+    playerLinks.innerHTML += 
+     `<div class="player" onclick="${player.function ? player.function : `window.open('player.url', '_blank');`}">
+        <img src="${player.logo}">
+        <span>${player.name}</span>
+      </div>`;
   }
 }
